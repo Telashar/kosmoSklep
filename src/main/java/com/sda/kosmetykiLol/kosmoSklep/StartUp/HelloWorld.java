dca8f56df3950466d5bbc6a766026ab4acf5bb31
LeszekBialy <wtlechu@gmail.com>
2019-06-30 13:53:36 +0200

    try

--- src/main/java/com/sda/kosmetykiLol/kosmoSklep/StartUp/HelloWorld.java
+++ src/main/java/com/sda/kosmetykiLol/kosmoSklep/StartUp/HelloWorld.java
@@ -2,6 +2,10 @@ package com.sda.kosmetykiLol.kosmoSklep.StartUp;
 
 public class HelloWorld {
     public static void main(String[] args) {
+
+
         System.out.println("Hello World!");
+
+        System.out.println("test");
     }
 }
