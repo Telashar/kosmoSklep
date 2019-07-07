2980392e9dd9feae4d5b616394d53a2d6ca3184d
Telashar <kaedeslawk@gmail.com>
2019-07-07 10:16:12 +0200

    dodana bazy danych

---
+++ src/main/resources/data.sql
@@ -0,0 +1,13 @@
+INSERT INTO product (id, price, name, description, available) VALUES ('1', '8', 'Apache', 'Combat Helicopter', '8');
+INSERT INTO product (id, price, name, description, available) VALUES ('2', '8', 'Tirpitz', 'KMS Battleship', '8');
+INSERT INTO product (id, price, name, description, available) VALUES ('3', '8', 'Baneblade', 'Tank', '8');
+INSERT INTO product (id, price, name, description, available) VALUES ('4', '1', 'GodEmperor', 'of Mankind', '1');
+--
+-- INSERT INTO owner(id, first_name, last_name, address, city, telephone) VALUES ('1', 'Jan', 'Kowalski', 'Al. Grunwaldzka', 'Gdansk', '123123123');
+-- INSERT INTO owner(id, first_name, last_name, address, city, telephone) VALUES ('2', 'Michał', 'Kowalski', 'Al. Niepodleglosci', 'Sopot', '123123123');
+
+
+INSERT INTO user (id, first_name, last_name, password, email, street_address1, street_address2, city, postal_code, state, country) VALUES ('1', 'Janusz', 'Kowalski', 'dobrehaslo10/10', 'megmail@.johan.com', 'fajnaulica', '16/2', 'Zelokowo','89-116', 'pomorskie', 'Niemcy');
+INSERT INTO user (id, first_name, last_name, password, email, street_address1, street_address2, city, postal_code, state, country) VALUES ('2', 'Johan', 'Kowalski', 'dobrehaslo10/10', 'megmail@.johan.com', 'fajnaulica', '16/2', 'Zelokowo','89-116', 'pomorskie', 'Niemcy');
+INSERT INTO user (id, first_name, last_name, password, email, street_address1, street_address2, city, postal_code, state, country) VALUES ('3', 'Wlodzimierz', 'Kowalski', 'dobrehaslo10/10', 'megmail@.johan.com', 'fajnaulica', '16/2', 'Zelokowo','89-116', 'pomorskie', 'Niemcy');
+INSERT INTO user (id, first_name, last_name, password, email, street_address1, street_address2, city, postal_code, state, country) VALUES ('4', 'Obiwan', 'Kenobi', 'dobrehaslo10/10', 'megmail@.johan.com', 'fajnaulica', '16/2', 'Zelokowo', 'pomorskie','89-116', 'Niemcy');
