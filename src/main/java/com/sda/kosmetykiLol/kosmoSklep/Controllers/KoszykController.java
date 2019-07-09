5d0d17b5a4a61ebd6cc07d6a320591a95201c239
Telashar <kaedeslawk@gmail.com>
2019-07-09 12:50:43 +0200

    finalowy commit z niedzieli

--- src/main/java/com/sda/kosmetykiLol/kosmoSklep/Controllers/KoszykController.java
+++ src/main/java/com/sda/kosmetykiLol/kosmoSklep/Controllers/KoszykController.java
@@ -1,14 +1,12 @@
 package com.sda.kosmetykiLol.kosmoSklep.Controllers;
 
 import com.sda.kosmetykiLol.kosmoSklep.Entities.Products.Product;
-import com.sda.kosmetykiLol.kosmoSklep.Repositories.ProductRepository;
 import com.sda.kosmetykiLol.kosmoSklep.Services.KoszykService;
 import com.sda.kosmetykiLol.kosmoSklep.Services.ProductService;
 import lombok.Data;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
@@ -37,10 +35,10 @@ public class KoszykController {
 
     }
 
-    @GetMapping("/delete/{id}")
-    public String deleteFromKoszyk(@PathVariable("id")Long id){
-        koszykService.deleteProductKoszyk(id);
-        return "redirect: koszyk";
+    @GetMapping("/delete/{index}")
+    public String deleteFromKoszyk(@PathVariable("index")int index){
+        koszykService.deleteProductKoszyk(index);
+        return "redirect:koszyk";
     }
 
     @GetMapping("/finalizekoszyk")
--- src/main/java/com/sda/kosmetykiLol/kosmoSklep/Controllers/ViewController.java
+++ src/main/java/com/sda/kosmetykiLol/kosmoSklep/Controllers/ViewController.java
@@ -1,22 +1,22 @@
-package com.sda.kosmetykiLol.kosmoSklep.Controllers;
-
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-@Configuration
-@Controller
-@EnableAutoConfiguration
-@ComponentScan
-public class ViewController {
-
-    @RequestMapping("/")
-    public String index() {
-        return "index.jsp";
-    }
-
-
-}
-
+//package com.sda.kosmetykiLol.kosmoSklep.Controllers;
+//
+//import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+//import org.springframework.context.annotation.ComponentScan;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//
+//@Configuration
+//@Controller
+//@EnableAutoConfiguration
+//@ComponentScan
+//public class ViewController {
+//
+//    @RequestMapping("/")
+//    public String index() {
+//        return "index.jsp";
+//    }
+//
+//
+//}
+//
