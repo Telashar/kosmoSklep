84382a64b2519b0a46b6ca95cc241be9eb260111
Telashar <kaedeslawk@gmail.com>
2019-07-13 09:22:46 +0200

    zmieniony viewcontroller

--- src/main/java/com/sda/kosmetykiLol/kosmoSklep/Controllers/ViewController.java
+++ src/main/java/com/sda/kosmetykiLol/kosmoSklep/Controllers/ViewController.java
@@ -4,19 +4,17 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-//@Configuration
+@Configuration
 @Controller
-//@EnableAutoConfiguration
-//@ComponentScan
+@EnableAutoConfiguration
+@ComponentScan
 public class ViewController {
 
-    @GetMapping("/index")
-    public String getMainPage() {
-
-        return "index";
+    @RequestMapping("/")
+    public String index() {
+        return "index.jsp";
     }
 
 
