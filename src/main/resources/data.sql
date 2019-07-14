e39aeacc6953ecb9dba9c478a877522ba9bc4da8
Telashar <kaedeslawk@gmail.com>
2019-07-14 11:26:57 +0200

    naprawiony blad edytowania produktu

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,11 @@
 INSERT INTO product (id, price, name, description, available) VALUES ('1', '11', 'Bourjois', 'Your Bourjois here', '100');
 INSERT INTO product (id, price, name, description, available) VALUES ('2', '5', 'Max Factor', 'Your Max Factor here', '500');
 INSERT INTO product (id, price, name, description, available) VALUES ('3', '8', 'Revlon', 'Your Revlon here', '250');
-INSERT INTO product (id, price, name, description, available) VALUES ('4', '15', 'Rimmel', 'Your Rimmel here', '80');
+INSERT INTO product (id, price, name, description, available) VALUES ('5', '15', 'Rimmel', 'Your Rimmel here', '80');
+INSERT INTO product (id, price, name, description, available) VALUES ('6', '88', 'Johan', 'Such Johan', '111');
+INSERT INTO product (id, price, name, description, available) VALUES ('7', '1', 'Tirpitz', 'Kriegsmarine Schiff', '80');
+INSERT INTO product (id, price, name, description, available) VALUES ('8', '101', 'North Korea', 'Work force', '1');
+INSERT INTO product (id, price, name, description, available) VALUES ('9', '811', 'Aliens', 'Area 51', '9000');
 --
 -- INSERT INTO owner(id, first_name, last_name, address, city, telephone) VALUES ('1', 'Jan', 'Kowalski', 'Al. Grunwaldzka', 'Gdansk', '123123123');
 -- INSERT INTO owner(id, first_name, last_name, address, city, telephone) VALUES ('2', 'Michał', 'Kowalski', 'Al. Niepodleglosci', 'Sopot', '123123123');
